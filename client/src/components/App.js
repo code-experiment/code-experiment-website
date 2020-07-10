f9f8e1dfd6879bd52a0ac788447b474ad9c57e05
Daniel Floyd <gleek.dan@gmail.com>
2020-07-10 14:22:31 -0600

    added material-ui and cleaned up folder structure and added a favicon

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello from App
+    </div>
+  );
+}
+
+export default App;
