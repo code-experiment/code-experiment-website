921d1de401ed01c26a1db3988a0128269717e478
Michale-Cortez <CortezDevelopement@gmail.com>
2020-07-10 17:38:15 -0600

    added basic styles and a dedicated style sheet for navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,18 +1,31 @@
 import React from 'react'
 
+import '../styles/NavStyles.css';
+
 export default () => {
   return (
-    <div>
-      <div>
+    <div className="nav-bar-container">
+      <div className="logo-container">
         Code Expirement
       </div>
 
-      <nav>
-        <ul>
-          <li>About</li>
-          <li>Events</li>
-          <li>Slack</li>
-          <li>Contact</li>
+      <nav className="links-outer-wrapper">
+        <ul className="links-inner-wrapper">
+          <div className="link-wrapper">
+            <a href="#about" className="link">About</a>
+          </div>
+
+          <div className="link-wrapper">
+            <a href="#events" className="link">Events</a>
+          </div>
+          
+          <div className="link-wrapper">
+            <a href="#slack" className="link">Slack</a>
+          </div>
+
+          <div className="link-wrapper">
+            <a href="#contact-form" className="link">Contact</a>
+          </div>
         </ul>
       </nav>
     </div>
