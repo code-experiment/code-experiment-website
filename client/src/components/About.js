a5d842a7a925cdbd997d8715ca1e0ba5b720c4b4
Michale-Cortez <CortezDevelopement@gmail.com>
2020-07-10 19:18:37 -0600

    added basic styles to the About Component

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default () => {
   return (
-    <div id="about" style={{ paddingTop: 80 }}>
-      <h1>
-        About
-      </h1>
+    <div id="about" style={{ paddingTop: 80, width: "100%" }}>
+      <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center", borderBottom: "1px solid #CCC" }}>
+        <h1>
+          About
+        </h1>
+      </div>
 
-      <div>
+      <div style={{ textAlign: "center", paddingTop: 20 }}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus accusantium quasi eos, quisquam accusamus quae cumque, possimus officia iste aperiam dolores aliquam quidem ducimus omnis. Suscipit voluptate tempore similique illo.
         Dolorum nesciunt porro illum nobis, accusantium officia tempora earum magni quisquam maxime eaque voluptate laboriosam odio, modi, aliquam numquam sapiente fuga explicabo! Qui, explicabo sint magni ad nisi temporibus deleniti.
         Optio fuga distinctio fugiat architecto ut dolore est facere culpa odit deleniti. Ducimus esse sed reprehenderit, rerum molestiae iusto nobis est? Illo pariatur placeat neque quasi cumque eius, eum fugit!
@@ -16,7 +18,7 @@ export default () => {
         Modi qui consectetur optio, illum neque cupiditate ad, voluptatibus repellendus maiores quidem soluta, repellat aliquam aut nihil suscipit perspiciatis natus maxime corporis doloribus molestias porro. Fuga beatae vel laudantium? Molestias.
         Quae consequuntur nemo qui quis minima veritatis. Illo, eos culpa nemo ex iste architecto laborum quae aut, accusamus optio animi sapiente aliquam incidunt beatae voluptas deserunt nobis nisi laboriosam eligendi!
         Quisquam voluptatum, ab et iste magni doloribus consequatur hic dignissimos accusamus dolorem necessitatibus delectus labore optio quibusdam nobis repellendus accusantium! Mollitia nisi, odit rerum tenetur iusto minima inventore temporibus iste.
-      </div>
     </div>
+  </div>
   )
 }
