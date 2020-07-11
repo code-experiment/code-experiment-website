94a021addeba2bfdaf2acde0f085d11010d54636
Michale-Cortez <CortezDevelopement@gmail.com>
2020-07-10 18:41:27 -0600

    brought in a background image for slack portion and added basic styles will need to finish later

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import '../styles/main.css';
 import About from './About';
 import Nav from './Nav';
 import UpcomingEvents from './UpcomingEvents';
+import SlackImage from '../images/slack-image.png'
 import ContactForm from './ContactForm';
 
 function App() {
@@ -12,14 +13,17 @@ function App() {
       <div className="container">
         <About />
         <UpcomingEvents />
-        <a
-          href="https://join.slack.com/t/codeassistworkspace/shared_invite/zt-frl94kyr-v7QepaQJKDG1NRh0vPdRhQ"
-          target="_blank"
-          id="slack"
-          rel="noopener noreferrer"
-        >
-          Join Slack
-        </a>
+        <div className="slack-background-image" style={{ background: `url(${SlackImage})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", height: 100, display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <a
+            href="https://join.slack.com/t/codeassistworkspace/shared_invite/zt-frl94kyr-v7QepaQJKDG1NRh0vPdRhQ"
+            target="_blank"
+            id="slack"
+            rel="noopener noreferrer"
+            style={{ width: 100, height: 40, border: "1px solid black", textAlign: "center", justifySelf: "center", alignSelf: "center" }}
+          >
+            Join Slack
+          </a>
+        </div>
         <ContactForm />
       </div>
     </div>
