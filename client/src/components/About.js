bf6e16f095db349205f7b06ea58dabfd97e0e6ab
Michale-Cortez <CortezDevelopement@gmail.com>
2020-07-12 15:47:36 -0600

    put about page styles into a file sheet and added some more to corrosponde with form styles

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
+import '../styles/AboutStyles.css'
+
 export default () => {
   return (
-    <div id="about" style={{ paddingTop: 80, width: "100%" }}>
-      <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center", borderBottom: "1px solid #CCC" }}>
+    <div id="about">
+      <div className="about-header">
         <h1>
           About
         </h1>
       </div>
 
-      <div style={{ textAlign: "center", paddingTop: 20 }}>
+      <div className="about-content">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus accusantium quasi eos, quisquam accusamus quae cumque, possimus officia iste aperiam dolores aliquam quidem ducimus omnis. Suscipit voluptate tempore similique illo.
         Dolorum nesciunt porro illum nobis, accusantium officia tempora earum magni quisquam maxime eaque voluptate laboriosam odio, modi, aliquam numquam sapiente fuga explicabo! Qui, explicabo sint magni ad nisi temporibus deleniti.
         Optio fuga distinctio fugiat architecto ut dolore est facere culpa odit deleniti. Ducimus esse sed reprehenderit, rerum molestiae iusto nobis est? Illo pariatur placeat neque quasi cumque eius, eum fugit!
