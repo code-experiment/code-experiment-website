4501f4ee21d5e521295938ec474a6a6c84409290
Brian Kozub <ticet11@gmail.com>
2020-07-16 14:13:07 -0600

    time displays, start button added.

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Timer = () => {
+    const [hours, setHours] = useState(5);
+    const [minutes, setMinutes] = useState(5);
+    const [seconds, setSeconds] = useState(5);
+
+    const handleStartClick = () => {
+        console.log("click");
+    };
+
     return (
         <div>
-            <h1>hello</h1>
+            <h1>
+                {hours < 10 ? `0${hours}` : { hours }}:
+                {minutes < 10 ? `0${minutes}` : { minutes }}:
+                {seconds < 10 ? `0${seconds}` : { seconds }}
+            </h1>
+
+            <button onClick={handleStartClick}>Start</button>
         </div>
     );
-}
+};
 
 export default Timer;
