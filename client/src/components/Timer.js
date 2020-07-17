6f4c5d598f98f31d9e1797f026ea21b8cc73566c
Brian Kozub <ticet11@gmail.com>
2020-07-16 20:47:31 -0600

    timer works with hardcoded time in seconds.

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 const Timer = () => {
     const [isRunning, setIsRunning] = useState(false);
-    const [currentTime, setCurrentTime] = useState(0)
-    const [timeLeft, setTimeLeft] = useState(5);
+    const [timeLeft, setTimeLeft] = useState(125);
 
     useEffect(() => {
         if (isRunning && timeLeft > 0) {
+            console.log(moment.duration(timeLeft, "seconds"));
             const interval = setInterval(
                 () => setTimeLeft(timeLeft - 1),
                 1000
@@ -22,7 +23,27 @@ const Timer = () => {
     return (
         <div>
             <h1>
-                {timeLeft}
+                {moment.duration(timeLeft, "seconds")._data.hours < 10
+                    ? `0${
+                          moment.duration(timeLeft, "seconds")._data
+                              .hours
+                      }`
+                    : moment.duration(timeLeft, "seconds")._data
+                          .hours}
+                :{moment.duration(timeLeft, "seconds")._data.minutes < 10
+                    ? `0${
+                          moment.duration(timeLeft, "seconds")._data
+                              .minutes
+                      }`
+                    : moment.duration(timeLeft, "seconds")._data
+                          .minutes}:
+                {moment.duration(timeLeft, "seconds")._data.seconds < 10
+                    ? `0${
+                          moment.duration(timeLeft, "seconds")._data
+                              .seconds
+                      }`
+                    : moment.duration(timeLeft, "seconds")._data
+                          .seconds}
             </h1>
 
             <button onClick={handleStartClick}>Start</button>
