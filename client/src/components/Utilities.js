9ea5830f957212c67b2c02ac3f465d6353b60d71
Daniel Floyd <gleek.dan@gmail.com>
2020-07-17 15:31:18 -0600

    added padding for utilities page

--- client/src/components/Utilities.js
+++ client/src/components/Utilities.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default () => {
   return (
-    <div>
+    <div className="utilities-wrapper">
       <h1>Utilities Page</h1>
       <Link to='/utilities/randomizer'>Randomizer</Link>
       <Link to='/utilities/timer'>Timer</Link>
