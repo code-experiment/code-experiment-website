b05e2e3fa1df8d8f8d7401d29685f4c8bfbc6791
Brian Kozub <ticet11@gmail.com>
2020-07-16 22:07:46 -0600

    added pause and resume and started react forms for input.

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import moment from "moment";
 
 const Timer = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+
+    const [isStarted, setIsStarted] = useState(false);
     const [isRunning, setIsRunning] = useState(false);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
     const [timeLeft, setTimeLeft] = useState(125);
 
     useEffect(() => {
@@ -16,37 +23,69 @@ const Timer = () => {
         }
     });
 
-    const handleStartClick = () => {
-        setIsRunning(true);
+    const handleClick = () => {
+        setIsRunning(!isRunning);
+        if (!isStarted) {
+            setIsStarted(!isStarted);
+        }
     };
 
     return (
         <div>
-            <h1>
-                {moment.duration(timeLeft, "seconds")._data.hours < 10
-                    ? `0${
-                          moment.duration(timeLeft, "seconds")._data
-                              .hours
-                      }`
-                    : moment.duration(timeLeft, "seconds")._data
-                          .hours}
-                :{moment.duration(timeLeft, "seconds")._data.minutes < 10
-                    ? `0${
-                          moment.duration(timeLeft, "seconds")._data
-                              .minutes
-                      }`
-                    : moment.duration(timeLeft, "seconds")._data
-                          .minutes}:
-                {moment.duration(timeLeft, "seconds")._data.seconds < 10
-                    ? `0${
-                          moment.duration(timeLeft, "seconds")._data
-                              .seconds
-                      }`
-                    : moment.duration(timeLeft, "seconds")._data
-                          .seconds}
-            </h1>
-
-            <button onClick={handleStartClick}>Start</button>
+            {!isStarted ? (
+                <form action="">
+                    <input
+                        type="text"
+                        placeholder="hours"
+                        name="hours"
+                    />
+                    <input
+                        type="text"
+                        placeholder="minutes"
+                        name="minutes"
+                    />
+                    <input
+                        type="text"
+                        placeholder="seconds"
+                        name="seconds"
+                    />
+                    <button onClick={handleClick}>Start</button>
+                </form>
+            ) : (
+                <div>
+                    <h1>
+                        {moment.duration(timeLeft, "seconds")._data
+                            .hours < 10
+                            ? `0${
+                                  moment.duration(timeLeft, "seconds")
+                                      ._data.hours
+                              }`
+                            : moment.duration(timeLeft, "seconds")
+                                  ._data.hours}
+                        :
+                        {moment.duration(timeLeft, "seconds")._data
+                            .minutes < 10
+                            ? `0${
+                                  moment.duration(timeLeft, "seconds")
+                                      ._data.minutes
+                              }`
+                            : moment.duration(timeLeft, "seconds")
+                                  ._data.minutes}
+                        :
+                        {moment.duration(timeLeft, "seconds")._data
+                            .seconds < 10
+                            ? `0${
+                                  moment.duration(timeLeft, "seconds")
+                                      ._data.seconds
+                              }`
+                            : moment.duration(timeLeft, "seconds")
+                                  ._data.seconds}
+                    </h1>
+                    <button onClick={handleClick}>
+                        {isRunning ? 'Pause' : 'Resume'}
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
