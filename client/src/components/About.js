1f0e73c5252bb6d321ca42860bb10c95f2755a3b
Michael Cortez <CortezDevelopement@gmail.com>
2020-07-31 00:05:18 -0600

    removed unused pieces of code in about component and about stylesheet

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -32,15 +32,6 @@ export default () => {
             </div>
           </div>
           <div className="about-secondary-content">
-            {/* <div className="about-secondary-content-item">
-              Welcome to Code Experiment. Our goal is to help all those at a beginner and intermediate level get a better grasp of web development by hands on experimentation with other coders. While most meetups have you follow along or try to code on your own after the fact, we believe that having a mentor accessible WHILE you’re coding can make all the difference.
-            </div>
-            <div className="about-secondary-content-item">
-              Dan Floyd, a current instructor and student mentor at Bottega specializing in Python, NodeJs and React, will lead each week’s experiment. You will be given challenges to complete either by yourself or by pair-programming with others in the meetup. Challenges will be sized for completion within the timeframe of each meetup, and there will be a slack channel for all meetup members to discuss and compare during the week.
-            </div>
-            <div className="about-secondary-content-item">
-              Every Wednesday at 6pm MT
-            </div> */}
             <img src={Image} alt="Image of Code" style={{ width: "100%" }} />
           </div>
         </div>
