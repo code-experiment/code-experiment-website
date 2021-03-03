646269a198b5c0b09088cfff01874969d352bfff
Daniel Floyd <gleek.dan@gmail.com>
2021-03-03 11:37:08 -0700

    Fix issue#65, add googleapis package
    I added the googleapis package and refactored the code to be more aligned with some of the docs mentioned in previous messages.

--- mail-server/routes/sendToMe.js
+++ mail-server/routes/sendToMe.js
@@ -1,59 +1,62 @@
-const express = require('express')
-const sendToMeRouter = express.Router()
-const nodemailer = require('nodemailer')
+const express = require("express");
+const sendToMeRouter = express.Router();
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const { OAuth2 } = google.auth;
+
+const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
+const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, THE_EMAIL } = process.env;
+const oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, OAUTH_PLAYGROUND);
+
+oauth2Client.setCredentials({
+  refresh_token: REFRESH_TOKEN,
+});
+
+const accessToken = oauth2Client
+  .getAccessToken()
+  .catch((err) => console.log(err));
 
 let transport = {
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    type: 'OAuth2',
-    user: process.env.THE_EMAIL,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
-    accessToken: process.env.ACCESS_TOKEN,
-    expires: 3599
-  }
-}
+    type: "OAuth2",
+    user: THE_EMAIL,
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    refreshToken: REFRESH_TOKEN,
+    accessToken: accessToken,
+    expires: 3599,
+  },
+};
 
 const transporter = nodemailer.createTransport(transport);
-  transporter.verify((error, success) => {
-    if(error) {
-      //if error happened code ends here
-      console.error(error)
-    } else {
-      //this means success
-      console.log('users ready to mail myself', success)
-    }
-  });
- ;
-
- sendToMeRouter.post('/', (req, res) => {
+sendToMeRouter.post("/", (req, res) => {
   //make mailable object
   const mail = {
-    from: process.env.THE_EMAIL,
-    to: process.env.THE_EMAIL,
+    from: THE_EMAIL,
+    to: THE_EMAIL,
     subject: req.body.subject,
     text: `
       from: ${req.body.name} 
       contact: ${req.body.email}
       message: ${req.body.text}
-    `
-  }
+    `,
+  };
 
   transporter.sendMail(mail, (err, data) => {
-    if(err) {
+    if (err) {
       res.json({
-        status: 'fail'
-      })
+        status: "fail",
+      });
     } else {
       res.json({
-        status: 'success',
-        data
-      })
+        status: "success",
+        data,
+      });
     }
-  })
+  });
 });
 
-module.exports = sendToMeRouter
+module.exports = sendToMeRouter;
