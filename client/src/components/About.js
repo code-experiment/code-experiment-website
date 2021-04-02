02285e660ed188b94bc7236e0288c82cd87bbfd4
Nathan Storrs <pressntr@gmail.com>
2021-04-01 19:29:26 -0600

    Revert "assigned a variable to the arrow function for the About.js component and moved the export default to the bottom initial commit not done issue#71"
    This reverts commit a2ec6e3a4f75c7d46d2388f5ab8704afc34f92c7.
    
    reverting back to code before these changes

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,33 +1,22 @@
-import React from "react";
+import React from 'react'
 
-const About = () => {
+export default () => {
   return (
     <div id="About" className="about-wrapper">
       <div className="about-container">
-        <div className="about-header">CODE EXPERIMENT</div>
+        <div className="about-header">
+          CODE EXPERIMENT
+        </div>
 
         <div className="about-text-wrapper">
           <div className="about-text">
-            Welcome to Code Experiment. Our goal is to help all those at a
-            beginner and intermediate level get a better grasp of web
-            development by hands on experimentation with other coders. While
-            most meetups have you follow along or try to code on your own after
-            the fact, we believe that having a mentor accessible WHILE you’re
-            coding can make all the difference.
+            Welcome to Code Experiment. Our goal is to help all those at a beginner and intermediate level get a better grasp of web development by hands on experimentation with other coders. While most meetups have you follow along or try to code on your own after the fact, we believe that having a mentor accessible WHILE you’re coding can make all the difference.
           </div>
           <div className="about-text">
-            Dan Floyd, a current instructor and student mentor at Bottega
-            specializing in Python, NodeJs and React, will lead each week’s
-            experiment. You will be given challenges to complete either by
-            yourself or by pair-programming with others in the meetup.
-            Challenges will be sized for completion within the timeframe of each
-            meetup, and there will be a slack channel for all meetup members to
-            discuss and compare during the week.
+            Dan Floyd, a current instructor and student mentor at Bottega specializing in Python, NodeJs and React, will lead each week’s experiment. You will be given challenges to complete either by yourself or by pair-programming with others in the meetup. Challenges will be sized for completion within the timeframe of each meetup, and there will be a slack channel for all meetup members to discuss and compare during the week.
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default About;
+  </div>
+  )
+}
