055472640f631889a3c2ed2b69333cfc5837feeb
Brian Kozub <ticet11@gmail.com>
2021-09-11 19:42:00 -0600

    NodeMailer functional again.

--- mail-server/routes/sendToMe.js
+++ mail-server/routes/sendToMe.js
@@ -3,10 +3,13 @@ const sendToMeRouter = express.Router();
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const { OAuth2 } = google.auth;
+// const config = require("../config.js");
+const process = require("process"); // though available globally, nodejs.org recommends importing process for each use
 
-const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
+const SCOPE = "https://mail.google.com/";
 const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, THE_EMAIL } = process.env;
-const oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, OAUTH_PLAYGROUND);
+// const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, THE_EMAIL } = config;
+const oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, SCOPE);
 
 oauth2Client.setCredentials({
     refresh_token: REFRESH_TOKEN,
@@ -17,9 +20,7 @@ const accessToken = oauth2Client
     .catch((err) => console.log(err));
 
 let transport = {
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    service: "gmail",
     auth: {
         type: "OAuth2",
         user: THE_EMAIL,
@@ -27,7 +28,6 @@ let transport = {
         clientSecret: CLIENT_SECRET,
         refreshToken: REFRESH_TOKEN,
         accessToken: accessToken,
-        expires: 3599,
     },
 };
 
@@ -35,20 +35,17 @@ const transporter = nodemailer.createTransport(transport);
 sendToMeRouter.post("/", (req, res) => {
     //make mailable object
     const mail = {
+        // from: `${req.body.name} <${req.body.email}>`,
         from: THE_EMAIL,
         to: THE_EMAIL,
         subject: req.body.subject,
-        text: `
-      from: ${req.body.name} 
-      contact: ${req.body.email}
-      message: ${req.body.text}
-    `,
+        text: `${req.body.text}\r\nSent from CodeExperiment Contact Form`,
     };
 
     transporter.sendMail(mail, (err, data) => {
         if (err) {
             res.json({
-                status: "fail",
+                status: `${err}`,
             });
         } else {
             res.json({
@@ -56,6 +53,7 @@ sendToMeRouter.post("/", (req, res) => {
                 data,
             });
         }
+        transporter.close();
     });
 });
 
