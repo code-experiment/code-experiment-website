8a3e51ce011d90218ac3beb5192d0489061369d3
Brian Kozub <ticet11@gmail.com>
2021-09-11 21:51:36 -0600

    Added config file, in case nodemailer needs more editing in the future. Values will just need to be added in when testing.

---
+++ mail-server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    THE_EMAIL: "codeexperimentcontact@gmail.com",
+    THE_PASSWORD: "PasswordHere",
+    CLIENT_ID: "ClientIDHere",
+    CLIENT_SECRET: "ClientSecretHere",
+    REFRESH_TOKEN: "RefreshTokenHere",
+    ACCESS_TOKEN: "AccessTokenHere",
+};
