e1a54ee0448e4da72d678ad14a51f023c1775f18
Brian Kozub <ticet11@gmail.com>
2021-09-12 14:36:30 -0600

    clean-config script checks config file and replaces if dirty.

---
+++ clean-config/index.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+
+// get the project folder path
+const projRootPath = __dirname.split("\\clean-config")[0].replace(/\\/g, "/");
+
+if (fs.existsSync(`${projRootPath}/mail-server/config.js`)) {
+    // If config.js exists
+    const config = require(`${projRootPath}/mail-server/config`);
+
+    if (
+        config === // If config.js is clean
+        {
+            THE_EMAIL: "codeexperimentcontact@gmail.com",
+            THE_PASSWORD: "PasswordHere",
+            CLIENT_ID: "ClientIDHere",
+            CLIENT_SECRET: "ClientSecretHere",
+            REFRESH_TOKEN: "RefreshTokenHere",
+            ACCESS_TOKEN: "AccessTokenHere",
+        }
+    ) {
+        console.log("config is clean");
+    } else {
+        // If config.js has been edited
+        console.log("config is not clean");
+        // Delete config.js file
+        fs.unlinkSync(`${projRootPath}/mail-server/config.js`);
+    }
+}
+
+if (!fs.existsSync(`${projRootPath}/mail-server/config.js`)) {
+    // If config.js does not exist
+    fs.writeFileSync(
+        // Create config.js and add structure with filler data
+        `${projRootPath}/mail-server/config.js`,
+        `module.exports = {
+	THE_EMAIL: "codeexperimentcontact@gmail.com",
+	THE_PASSWORD: "PasswordHere",
+	CLIENT_ID: "ClientIDHere",
+	CLIENT_SECRET: "ClientSecretHere",
+	REFRESH_TOKEN: "RefreshTokenHere",
+	ACCESS_TOKEN: "AccessTokenHere",
+};
+`
+    );
+    console.log("config.js has been rewritten without secrets");
+}
